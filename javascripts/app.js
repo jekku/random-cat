2b23f7072e777e62179fc64b00d1277a3cc96bd3
jekku <jekripreclaroorlina@gmail.com>
2017-01-05 09:37:42 +0800

    Adds button for adding more cats

---
+++ javascripts/app.js
@@ -0,0 +1,7 @@
+$('#addPussy').click(function () {
+  const pussyImg = $('img').clone()[0];
+  /* Not sure what to do here :(
+   * Maybe something like:
+   * $('#catContainer').append();
+   */
+});
