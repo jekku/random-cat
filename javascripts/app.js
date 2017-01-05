19137702215c0d64f6dcae04f958568fcaa29581
rossst <rossssstungol@gmail.com>
2017-01-05 14:07:50 +0800

    Adds image on button click

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,4 @@
 $('#addPussy').click(function () {
   const pussyImg = $('img').clone()[0];
-  /* Not sure what to do here :(
-   * Maybe something like:
-   * $('#catContainer').append();
-   */
+  $('#catContainer').append('<img src="http://thecatapi.com/api/images/get?format=src&type=gif"/>')
 });
